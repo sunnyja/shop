248abdbddf4c55c105c059bcc6d05a71b108fe34
sunnyja <45287732+sunnyja@users.noreply.github.com>
2019-09-27 12:27:20 +0300

    Update index.php
    i

--- shop/public/index.php
+++ shop/public/index.php
@@ -1,1 +1,19 @@
-<?phpfunction __autoload($classname){	include_once("../controllers/$classname.class.php");}$action = 'action_';$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';if (isset($_GET['c'])) {	if ($_GET['c'] == 'page') {		$controller = new Page();	} else if ($_GET['c'] == 'user') {		$controller = new User();	} else if ($_GET['c'] == 'admin') {	    $controller = new Admin();    }}else {	$controller = new Page();}$controller->Request($action); //в функцию Request (controller.class.php) передается значение action_act
+<?php
+function __autoload($classname){
+	include_once("../controllers/$classname.class.php");
+}
+$action = 'action_';
+$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';
+
+if (isset($_GET['c'])) {
+	if ($_GET['c'] == 'page') {
+		$controller = new Page();
+	} else if ($_GET['c'] == 'user') {
+		$controller = new User();
+	} else if ($_GET['c'] == 'admin') {
+	    $controller = new Admin();
+    }
+} else {
+	$controller = new Page();
+}
+$controller->Request($action); //в функцию Request (controller.class.php) передается значение action_act
